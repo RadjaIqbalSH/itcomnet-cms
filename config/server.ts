5187824840ba23815a0d6fb2d3d31801ae63ddc7
FD-Iqbal <radja@femaledaily.com>
2024-06-16 03:42:04 +0700

    init

--- config/server.ts
+++ config/server.ts
@@ -4,7 +4,7 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // url: "/dashboard",
+  url: env("STRAPI_ADMIN_BACKEND_URL", "http://localhost:1337"),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
